7dabb34c12cb29cdce361363cc0ed6bd51294124
Graydon Langford <graydonl92@yahoo.com>
2016-08-13 12:06:47 -0500

    removed semicolons again and changed capitalization of require('Sequelize') to require('sequelize')

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
-var Sequelize = require('Sequelize');
-var env = process.env.NODE_ENV || 'development';
-var sequelize;
+var Sequelize = require('sequelize')
+var env = process.env.NODE_ENV || 'development'
+var sequelize
 
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
-	});
+	})
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect':'sqlite',
 	'storage': __dirname + '/data/dev-todo-api.sqlite'
-	});
+	})
 }
 
 var db = {}
 
-db.todo = sequelize.import(__dirname + '/models/todo.js');
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.todo = sequelize.import(__dirname + '/models/todo.js')
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
