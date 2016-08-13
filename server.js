4efdfbedbc3b79e1408b892bf8dd23d641b0783c
Graydon Langford <graydonl92@yahoo.com>
2016-08-13 12:48:10 -0500

    convert DELETE /todos/:id to use sql

--- server.js
+++ server.js
@@ -8,65 +8,16 @@ var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// var todoModel = {
-//   id: {
-//     type: 'number',
-//     searchType: 'exact'
-//   },
-//   complete: {
-//     type: 'boolean',
-//     searchType: 'exact'
-//   },
-//   description: {
-//     type: 'string',
-//     searchType: 'includes'
-//   },
-// }
-
-// var todos = [{
-//   id: 1,
-//   complete: false,
-//   description: 'item 1'
-// }, {
-//   id: 2,
-//   complete: true,
-//   description: 'item 2',
-// }];
-
-// var todoNextId = 3;
-
 var validKeys = ['description', 'complete', 'id'];
 
-// coerceToModelType = function(obj) {
-//   var keys = _.keys(obj);
-
-//   for (i = 0, l = keys.length; i < l; i++) {
-
-//     var key = keys[i]
-
-//     if (todoModel[key].type === 'boolean') {
-//       if (obj[key].toLowerCase().trim() === 'true') {
-//         obj[key] = true;
-//       } else if (obj[key].toLowerCase().trim() === 'false') {
-//         obj[key] = false;
-//       }
-//     } else if (todoModel[key].type === 'number') {
-//       if (!isNaN(parseInt(obj[key]))) {
-//         obj[key] = parseInt(obj[key]);
-//       }
-//     } else if (todoModel[key].type === 'string') {
-//       obj[key].toString();
-//     }
-//   }
-//   return obj;
-// }
-
 app.use(bodyParser.json());
+
 
 //root
 app.get('/', (req, res) => {
   res.send('Todo Api Root');
 });
+
 
 //get all todos
 app.get('/todos', (req, res) => {
@@ -92,33 +43,8 @@ app.get('/todos', (req, res) => {
     res.status(500).send()
   })
 
-  // if (req.query) {
-  //   var query = _.pick(req.query, validKeys); // trim any query params not in the accepted params list
-  //   query = coerceToModelType(query); // coerce each query value to the type of it's param in the todoModel object
-  //   selectedTodos = todos; // set selectedTodos to todos so we don't modify the actual todos object accidentally
-
-  //   var queryKeys = _.keys(query); //create array of all the params in the query string
-
-  //   for (i = 0, l = queryKeys.length; i < l; i++) { // for each param in query
-
-  //     var queryKey = queryKeys[i] //current query key
-  //     var queryItem = query[queryKey] //current query "item"
-  //     var todoModelProperty = todoModel[queryKey] // todoModel property matching this query
-
-  //     selectedTodos = _.filter(selectedTodos, (todo) => { //filter the selectedTodos based off the criteria from the current query param
-  //       if (todoModel[queryKey].searchType === 'exact') { //if exact match
-  //         return queryItem === todo[queryKey]; // then check if each todo (currently selected param) matches the current param's search exactly
-  //       } else if (todoModel[queryKey].searchType === 'includes') { // if an 'includes' search
-  //         return todo[queryKey].toLowerCase().indexOf(queryItem.toLowerCase()) > -1  // then check if each todo (currently selected param) exists anywhere in the current query param
-  //       } else { //otherwise, there's something wrong with the todoModel, and we should return everything
-  //         return true;
-  //       }
-  //     });
-  //   }
-  // }
-
-  // res.json(selectedTodos);
 });
+
 
 // GET todos/:id
 app.get('/todos/:id', (req, res) => {
@@ -134,61 +60,40 @@ app.get('/todos/:id', (req, res) => {
     res.status(500).send()
   })
 
-
-  // var todoId = parseInt(req.params.id, 10),
-  //   todo = _.findWhere(todos, {
-  //     id: todoId
-  //   });
-
-  // if (todo) {
-  //   res.json(todo);
-  // } else {
-  //   res.status(404).send('todo not found with id of ' + todoId);
-  // }
 });
+
 
 //post a todo
 app.post('/todos', (req, res) => {
   var body = _.pick(req.body, validKeys);
-
-  //call create on db.todo
-  //  respond with 200 and todo
-  //  res.status(400).json(e)
 
   db.todo.create(body).then((todo)=>{
     res.json(todo.toJSON());
   },(e)=>{
     res.status(400).json(e);
   });
-
-  // if (!_.isBoolean(body.complete) || !_.isString(body.description) || body.description.trim().length === 0) {
-  //  return res.status(400).send();
-  // }
-
-  // body.description = body.description.trim();
-
-  // body.id = todoNextId++;
-
-  // todos.push(body);
-
-  // res.json(body);
 });
+
 
 //delete a todo
 app.delete('/todos/:id', (req, res) => {
-  var todoId = parseInt(req.params.id, 10),
-    todo = _.findWhere(todos, {
-      id: todoId
-    });
 
-  if (!todo) {
-    return res.status(404).json({
-      "error": "no todo found with id: " + todoId
-    });
-  }
+  var todoId = parseInt(req.params.id, 10)
 
-  todos = _.without(todos, todo);
-  res.json(todo);
+  db.todo.findById(todoId).then(function (todo) {
+    todo.destroy().then(
+      function (todo) {
+        res.json(todo)
+      },
+      function (err) {
+        res.status(500).send()
+      }
+    )
+  }, function (err) {
+    res.status(404).json({
+      error: 'no todo found with id: ' + todoId
+    })
+  })
 })
 
 //edit a todo
